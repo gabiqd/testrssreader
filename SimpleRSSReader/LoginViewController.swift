cd7763ec5447234ec6ad31fc3dc1f771dbe7a060
Gabi <gabi@cielabs.com>
2020-09-20 18:30:48 -0300

    Add logic to run UI code

---
+++ SimpleRSSReader/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  SimpleRSSReader
+//
+//  Created by Gabriel on 20/09/2020.
+//  Copyright © 2020 Gabriel. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
